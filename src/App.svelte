6b801e32f418650c80fa70f3407b1d579de11bf2
torkilef <torkilef@uio.no>
2022-10-01 14:03:44 +0200

    Forste commit, test for github pages

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+<div>
+  <header>
+    <div>
+
+    </div>
+    <h1>
+      Hei, siden er under utvikling, kom heller tilbake senere!
+    </h1>
+    <div>
+      
+    </div>
+    
+  </header>
+</div>
+
+
+
+<style>
+
+header{
+  display: flex;
+  flex-direction: row;
+  min-height: 80px;
+  align-items: center;
+  justify-content:space-between;
+  background-color:rgb(236, 236, 236);
+  }
+
+</style>
